49832dd3228f585a798a6079c5c66a9ba55c8ee1
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-11 20:43:47 +0900

    cancel button

--- src/Component/Post/Detail.js
+++ src/Component/Post/Detail.js
@@ -1,6 +1,6 @@
 import React , {useState,useEffect} from 'react';
 import {Link} from "react-router-dom";
-import {useParams} from "react-router-dom";
+import {useParams,useNavigate} from "react-router-dom";
 import axios from 'axios';
 
 import {Spinner} from "react-bootstrap";
@@ -8,6 +8,7 @@ import { PostDiv, Post, BtnDiv, SpinnerDiv } from "../../Style/PostDetailCSS.js"
 
 function Detail() {
     let params = useParams();
+    let navigate =useNavigate();
     const [PostInfo, setPostInfo] =useState({})
     const [Flag, setFlag] = useState(false)
     useEffect(()=>{
@@ -29,6 +30,25 @@ function Detail() {
     useEffect(()=>{
       console.log(PostInfo);
     },[PostInfo])
+
+    const DeleteHandel = () => {
+      if(window.confirm("정말로 삭제하시겠습니까?")){
+        let body={
+          postNum : params.postNum,
+      }
+     axios
+     .post("/api/post/delete" ,body)
+     .then((response)=>{
+       if(response.data.success){
+        alert("게시글이 삭제되었습니다")
+        navigate("/");
+       }  
+     })
+     .catch((err)=>{
+      alert("게시글이 삭제에 실패 하였습니당")
+     })
+      }
+    }
   return (
     <PostDiv>
       {Flag ? (
@@ -40,7 +60,8 @@ function Detail() {
         </Post>
         <BtnDiv>
           <Link to={`/edit/${PostInfo.postNum}`}><button className="edit">수정</button></Link>
-          <button className="delete">삭제</button>
+          <button className="delete" 
+          onClick={()=>DeleteHandel()}>삭제</button>
         </BtnDiv>
         </div>
         ):(
