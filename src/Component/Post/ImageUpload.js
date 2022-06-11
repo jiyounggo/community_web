f0822e4f78f670092b9680e12028f9e655008377
leejiyoung <103088450+jiyounggo@users.noreply.github.com>
2022-06-11 22:15:21 +0900

    Uploading images to the server

--- src/Component/Post/ImageUpload.js
+++ src/Component/Post/ImageUpload.js
@@ -1,10 +1,29 @@
 import React from "react";
-import {Form} from "react-bootstrap"
+import {Form} from "react-bootstrap";
+import axios from "axios";
 
 function ImageUpload() {
+
+// 1.사용자가 이미지를 업로드하면
+// 2.업로드한 이미지를 받아서 서버에 저장
+// 3.저장한 이미지 경로를 다시 클라이언트에게 전송
+// 4.경로를 받아서 post model에 저장
+
+const FileUpload = (e) => {
+    var formData =new FormData();
+    formData.append("file",(e.target.files[0]));
+    axios.post("/api/post/image/upload",formData)
+}
+
   return (
     <div>
-        <Form.Control type="file" className="shadow-none"/>
+        <Form.Control type="file" className="shadow-none"
+         accept="image/*"
+         onChange={(e)=>{
+            FileUpload(e)
+         }}
+         
+         />
 
     </div>
   )
